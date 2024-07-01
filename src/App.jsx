48fe885b210c35444dcc7ebee605d71327ec9bce
salah <salahakon1998@gmail.com>
2024-07-01 13:22:43 +0000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
 import AppLayout from "./components/base/AppLayout";
 
 function App() {
-	return <AppLayout />;
+	return (
+		<BrowserRouter>
+			<AppLayout />
+		</BrowserRouter>
+	);
 }
 
 export default App;
