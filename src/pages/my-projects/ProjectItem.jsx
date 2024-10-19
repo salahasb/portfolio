3e0a624bbf6ccd6935cd638df5a58074bd2c969c
salah <salahakon1998@gmail.com>
2024-10-19 15:27:57 +0100

    Add "wajbaty" app screenshot in the slider

--- src/pages/my-projects/ProjectItem.jsx
+++ src/pages/my-projects/ProjectItem.jsx
@@ -7,24 +7,26 @@ function ProjectItem({ index, project, currentItem, setCurrentItem }) {
 
 	// console.log(isCurrentItem, index);
 	return (
+		// for another effect set "50" to the 4th argument of "cubic" css function
 		<li
-			className=" w-[50%]  700:w-[40%]  xl:w-[26.4%] shrink-0  aspect-video     transition-[transform] duration-[0.6s]   ease-[cubic-bezier(.5,0,0,1)]  cursor-pointer @container   "
+			className=" w-[50%]  700:w-[40%]  xl:w-[26.4%] shrink-0  aspect-video transition-[transform_opacity] duration-[0.6s]    cursor-pointer @container ease-[cubic-bezier(.5,0,0.40,1)] opacity-[.5]  "
 			style={
 				isCurrentItem
 					? {
 							transform: "scale(1.6)",
+							opacity: 1,
 
-							// for another effect set "50" to the 4th argument of "cubic" css function
-							transition: "transform 0.6s    cubic-bezier(.5,0,0.0,1) ",
+							// transition: "transform 0.6s    cubic-bezier(.5,0,0.0,1) ",
 					  }
 					: {}
 			}
 			onClick={handleOnClick}
 		>
-			<div className="border border-1 overflow-hidden size-full rounded-[10cqw]">
-				{project.name}
+			<div className="  border-1 overflow-hidden size-full rounded-[8cqw]">
+				{/* {project.name} */}
+				{/* <img src={`${project.src}-big.webp`} alt="" /> */}
+				<img src={`/projects/wajbaty-big.webp`} alt="" />
 			</div>
-			{/* <img src="/projects/the-wild-oasis.png" alt="" /> */}
 		</li>
 	);
 }
