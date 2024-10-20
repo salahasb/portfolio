68d3c0c9f2d0873e984023469906e7ec9da21ee9
salah <salahakon1998@gmail.com>
2024-10-20 18:03:44 +0100

    Adjust project techstack items

--- src/pages/my-projects/ProjectItemTitle.jsx
+++ src/pages/my-projects/ProjectItemTitle.jsx
@@ -1,7 +1,7 @@
 function ProjectItemTitle({ project }) {
 	return (
 		<div className=" ">
-			<h3 className={`mb-[0.5cqh] tracking-wide      text-[5.8cqi]    `}>
+			<h3 className={`mb-[0.8cqw] tracking-wide      text-[5.8cqi]    `}>
 				<span className="  font-semibold  ">{project.name}</span>
 
 				<span className="text-[3.8cqi] uppercase">
