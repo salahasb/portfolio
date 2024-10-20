8f37b7b7f788a0bcaf0cd19389c22a31d966cb85
salah <salahakon1998@gmail.com>
2024-10-20 18:31:28 +0100

    Adjust ProjectTechstack icons

--- src/pages/my-projects/ProjectTechstack.jsx
+++ src/pages/my-projects/ProjectTechstack.jsx
@@ -3,7 +3,7 @@ function ProjectTechstack({ project }) {
 		<ul className="flex  @container ">
 			{project.techStack.map((tool) => (
 				<li
-					className={`   size-[4.8%] aspect-square max-w-[1.8rem]       mr-[-0.25rem] text-[0.2rem]  bg-brand-900  rounded-full bg-no-repeat  border-[#888] bg-[length:50%] bg-[50%_50%] border`}
+					className={`    flex-grow-0 flex-shrink-0 basis-[4.8%] aspect-square max-w-[1.8rem]       mr-[-0.25rem] text-[0.2rem]  bg-brand-900  rounded-full bg-no-repeat  border-[#888] bg-[length:50%] bg-[50%_50%] border`}
 					key={tool}
 					style={{ backgroundImage: `url('/icons/${tool}.png')` }}
 				>
