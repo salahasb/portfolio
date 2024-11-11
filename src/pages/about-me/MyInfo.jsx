52d9e9a728424f0984f97f313aa7dc86faca2802
salah <salahakon1998@gmail.com>
2024-11-11 17:11:20 +0100

    Edit My info and add google analytics

--- src/pages/about-me/MyInfo.jsx
+++ src/pages/about-me/MyInfo.jsx
@@ -19,6 +19,7 @@ function MyInfo() {
 				</p>
 			</div>
 
+			{/* Currently, I’m deepening my front-end expertise by learning Next.js, TypeScript, and testing frameworks. My goal is to secure a front-end role where I can collaborate with a team on larger-scale applications, with future ambitions in business and entrepreneurship. */}
 			<div className="text-[1.5rem] [&>*]:mb-6 min-w-[28rem]  max-w-[36rem] xl:max-w-[40rem] lg:text-[1.6rem] text-[#ddd] leading-relaxed ">
 				<p className="opacity-0 animate-[fadeIn-translateUp_0.8s_0.5s_forwards]">
 					Hello! I&apos;m Salah Aassab, a front-end web developer specializing
@@ -26,14 +27,15 @@ function MyInfo() {
 				</p>
 
 				<p className="opacity-0 animate-[fadeIn-translateUp_0.8s_0.7s_forwards]">
-					Since 2020, I&apos;ve been honing my skills in HTML, CSS, and
-					JavaScript, with a particular focus on building responsive and dynamic
-					web applications.
+					Since 2021, I&apos;ve built a solid foundation in front-end
+					development with deep knowledge in JavaScript and React. with a strong
+					foundation in responsive design, clean code, and component-based
+					architecture.
 				</p>
 				<p className="opacity-0 animate-[fadeIn-translateUp_0.8s_0.9s_forwards]">
-					I&apos;ve completed several projects showcasing my expertise in React
-					and am currently expanding my skills towards becoming a MERN stack
-					developer.
+					I&apos;ve completed several projects showcasing my expertise in React.
+					and My goal is to secure a front-end role where I can collaborate with
+					a team on larger-scale applications.
 				</p>
 				<p className="opacity-0 animate-[fadeIn-translateUp_0.8s_1.1s_forwards]">
 					Let&apos;s work together to bring your project to life with clean,
